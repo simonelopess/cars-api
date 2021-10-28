f6cd0c25d5799fc4cf0e4aa1ec69261aa7a6a2f0
Simone <sisilopes20@gmail.com>
2021-10-28 19:28:06 -0300

    Atualizacao de categories

--- dist/routes/categories.routes.js
+++ dist/routes/categories.routes.js
@@ -2,14 +2,19 @@
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.categoriesRoutes = void 0;
 var express_1 = require("express");
+var Category_1 = require("../model/Category");
 var categoriesRoutes = (0, express_1.Router)();
 exports.categoriesRoutes = categoriesRoutes;
 var categories = [];
-categoriesRoutes.post("/categories", function (request, response) {
+categoriesRoutes.post("/", function (request, response) {
     var _a = request.body, name = _a.name, description = _a.description;
-    categories.push({
+    var category = new Category_1.Category();
+    //instancia um novo objeto
+    Object.assign(category, {
         name: name,
         description: description,
+        created_at: new Date(),
     });
-    return response.status(201).send();
+    categories.push(category);
+    return response.status(201).json({ category: category });
 });
--- dist/server.js
+++ dist/server.js
@@ -8,5 +8,5 @@ var categories_routes_1 = require("./routes/categories.routes");
 var PORT = process.env.PORT || 8080;
 var app = (0, express_1.default)();
 app.use(express_1.default.json());
-app.use(categories_routes_1.categoriesRoutes);
+app.use("/categories", categories_routes_1.categoriesRoutes);
 app.listen(PORT, function () { return console.log("Server is running!"); });
